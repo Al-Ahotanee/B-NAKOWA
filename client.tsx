3ad86e17622b8031187528fed30ef1450e964ede
Al-Ahotanee <60036086+Al-Ahotanee@users.noreply.github.com>
2026-08-25 22:15:28 +0100

    Add files via upload

--- client.tsx
+++ client.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createTRPCReact } from "@trpc/react-query";
